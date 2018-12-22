42eedec67af01a23065e65f373b356e9d4bd816e
malinroot <kurchevskijpavel@gmail.com>
2018-12-22 20:32:11 +0300

    Add files via upload

---
+++ finddup.py
@@ -0,0 +1,21 @@
+import collections
+import os 
+import sys
+
+path = sys.argv[1] if len(sys.arvg) > 1 else "."
+data = collections.defaultdict(list)
+
+for root, dirs, files in os.walk(path):
+    for filename in files:
+        fullname = os.path.join(root, filename)
+        key = (os.path.getsize(fullname), filename)
+        data[key].append(fullname)
+
+
+for size, filename in sorted(data):
+    names = data[(size, filename)]
+    if len(names) > 1:
+        print("{filename} ({size} bytes) may be duplicated"
+            "({0} files):".format(len(names), **locals()))
+    for name is names:
+        print("\t{0}".format(name))
